e358c29dc1556ab2a1522986050a91d43f45d6c0
peterjxl <peterjxl@qq.com>
2022-09-14 07:57:31 +0800

    王爽汇编实验13

--- wangs/lab13_2.asm
+++ wangs/lab13_2.asm
@@ -14,13 +14,13 @@ start:	mov ax, 0b800h
 		mov es, ax
 		mov di, 160*12
 		
-		mov bx, offset s - offset se
+		mov bx, offset s - offset se 	;设置从标号se到标号s的转移位移
 		mov cx, 80
 		
 s:		mov byte ptr es:[di], '!'
 		add di, 2
 		int 7ch
-se:		nop		
+se:		nop		 ;如果(cx) #=0,转移到标号s处
 
 		mov ax, 4c00h
 		int 21h
--- wangs/lab13_3.asm
+++ wangs/lab13_3.asm
@@ -1,26 +1,36 @@
-; 王爽汇编实验13 （2）编写并安装int 7ch中断例程，功能为完成loop指令的功能。 
-; 参数：（cx）=循环次数，（bx）=位移。
+; 王爽汇编实验13的中断安装程序（3）下面的程序，分别在屏幕的第2、4、6、8行显示4句英文诗，补全程序。
+assume cs:codesg
 
-
-assume cs:codesg, ds:datasg
-
-datasg segment
-	db "welcome to masm!", 0
-datasg ends
 
 
 codesg segment
-start:	mov ax, 0b800h
-		mov es, ax
-		mov di, 160*12
+	s1: db 'Good,better,best,','$'
+	s2: db 'Never let it rest,','$'
+	s3: db 'Till good is better','$'
+	s4: db 'Ant better,best,','$'
+	s:  dw offset s1, offset s2, offset s3, offset s4
+	row: db 2, 4, 6, 8
+	
+
+start:	mov ax, cs
+		mov ds, ax
+		mov bx, offset s
+		mov si, offset row
+		mov cx, 4
 		
-		mov bx, offset s - offset se
-		mov cx, 80
+ok:		mov bh, 0
+		mov dh, cs:[si] ;  mov dh, ____填空
+		mov dl, 0
+		mov ah, 2
+		int 10h
 		
-s:		mov byte ptr es:[di], '!'
-		add di, 2
-		int 7ch
-se:		nop		
+		mov dx, cs:[bx] ; mov dx, ____填空
+		mov ah, 9
+		int 21h
+		
+		add bx, 2 ; 填空
+		inc si ; 填空
+		loop ok
 
 		mov ax, 4c00h
 		int 21h
