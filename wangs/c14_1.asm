e5b6bb359e8ad6f62d8c3188ad289d2807c2ea15
peterjxl <peterjxl@qq.com>
2022-09-14 21:28:34 +0800

    检测点14.1和14.2

---
+++ wangs/c14_1.asm
@@ -0,0 +1,15 @@
+; 检测点 14.1
+; (1) 编程，读取CMOS RAM的2号单元的内容。
+; (2) 编程，向CMOS RAM的2号单元写入0。
+
+assume cs:codesg
+
+
+codesg segment
+start:	mov al, 2
+		out 70h, al
+		in al,71h
+		mov ax, 4c00h
+		int 21h
+codesg ends
+end start
